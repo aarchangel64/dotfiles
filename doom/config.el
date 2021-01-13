3edb5c951ce42d2c6dba01a9ea336bc58c8a3879
Shrey Pasricha <shrey.pasricha@gmail.com>
2021-01-12 19:49:39 -0500

    doom config tweaks

--- doom/config.el
+++ doom/config.el
@@ -26,7 +26,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. 'doom-one' is the default. doom-henna is also quite nice
-(setq doom-theme 'doom-horizon)
+(setq doom-theme 'doom-henna)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
