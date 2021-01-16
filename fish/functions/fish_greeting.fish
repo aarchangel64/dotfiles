0a6c946b8d6bea83f2507f73bacafbb7e2b8f5fa
Shrey Pasricha <shrey.pasricha@gmail.com>
2021-01-16 10:36:10 -0500

    changed fish greeting. its big and colourful now. no cows at the moo-ment.

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,3 +1,6 @@
 function fish_greeting
-fortune -a | cowsay | lolcat -t -F .2
+         fortune -es -n 26 | toilet -f pagga -F border -t | lolcat -t -F .2
+         # echo "$hostname" | toilet -f pagga -F border -t | lolcat -t -F .2
+         # neofetch
+         __draw-divider
 end
