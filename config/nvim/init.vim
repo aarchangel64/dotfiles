7f4354de4170657fbe9045857a18aabf9d6d0b6a
Maya Pasricha <shrey.pasricha@gmail.com>
2024-09-16 22:25:38 -0400

    Config updates

--- config/nvim/init.vim
+++ config/nvim/init.vim
@@ -10,6 +10,7 @@ call plug#begin(stdpath('data') . '/plugged')
 
 Plug 'easymotion/vim-easymotion'
 Plug 'chrisbra/vim-commentary'
+Plug 'tpope/vim-surround'
 
 " Initialize plugin system
 call plug#end()
--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,6 +1,21 @@
+# Rootless Docker
+set -gx DOCKER_HOST unix://$XDG_RUNTIME_DIR/docker.sock
+
+# bat-extras
+# batman to replace man
+export MANPAGER=env\ BATMAN_IS_BEING_MANPAGER=yes\ bash\ /usr/bin/batman
+export MANROFFOPT=-c
+
 # Starship prompt config (from https://github.com/starship/starship/discussions/2046#discussioncomment-274553)
 # disable virtualenv prompt, it breaks starship
 # set VIRTUAL_ENV_DISABLE_PROMPT 1
 
 starship init fish | source
 source /etc/grc.fish
+
+# pnpm
+set -gx PNPM_HOME "/home/cosmicdoge/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
